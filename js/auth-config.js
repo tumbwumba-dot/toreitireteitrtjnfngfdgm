d53a09c446fa8ae9e3f6890986dda8b06f37afd8
MortexSchmidt <neruka783@gmail.com>
2025-11-01 18:09:00 +0200

    Add trailing slash to redirect URI

--- js/auth-config.js
+++ js/auth-config.js
@@ -1,6 +1,6 @@
 const AUTH_CONFIG = {
     discord: {
         clientId: '1434129915502133249',
-        redirectUri: 'https://zoneblast.netlify.app'
+        redirectUri: 'https://zoneblast.netlify.app/'
     }
 };
